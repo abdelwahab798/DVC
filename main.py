f4e778d82f39db8c295ef5cd284de1b6adc2d14b
Abdelwahab <abdelwahabamr769@gmail.com>
2026-01-24 06:22:52 +0200

    third version of data

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import pandas as pd
 import os 
 
 df=pd.read_csv(r"C:\Users\nice\Desktop\Mlops\DVC\data\data.csv")
-new={"name":"g1",
+new={"name":"abdelrahamn",
      "country":"qatar",
       "age":45}
 df.loc[len(df.index)]=new
+new2={"name":"sdf1",
+     "country":"jordan",
+      "age":62}
+df.loc[len(df.index)]=new2
 df.to_csv(r"C:\Users\nice\Desktop\Mlops\DVC\data\data.csv", index=False)
